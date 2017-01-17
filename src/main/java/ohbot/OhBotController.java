4f006b0ebab97aaf8310e0f7e5d74b39212289bb
showofrce <showforce19@gmail.com>
2017-01-17 22:31:31 +0800

    test3

--- src/main/java/ohbot/OhBotController.java
+++ src/main/java/ohbot/OhBotController.java
@@ -63,157 +63,168 @@ public class OhBotController {
     }
 
     @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        log.info("Received message(Ignored): {}", event);
+    }
+
+    @EventMapping
     public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws IOException {
+
         handleTextContent(event.getReplyToken(), event, event.getMessage());
     }
 
     private void handleTextContent(String replyToken, Event event, TextMessageContent content) throws IOException {
         String text = content.getText();
-        if (text.contains("天氣") && text.contains("?")) {
+        if (text.endsWith("天氣?")) {
             text = text.replace("天氣", "").replace("?", "").replace("臺", "台").trim();
-            CloseableHttpClient httpClient = HttpClients.createDefault();
-            String strResult ;
-            switch (text) {
-                case "台北市": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_63.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
+            if (text.length() == 3) {
+
+
+                CloseableHttpClient httpClient = HttpClients.createDefault();
+                String strResult;
+                switch (text) {
+                    case "台北市": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_63.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "新北市": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_65.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "桃園市": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_68.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "臺中市": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_66.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "高雄市": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_64.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "基隆市": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10017.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "新竹市": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10018.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "新竹縣": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10004.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "苗栗縣": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10005.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "彰化縣": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10007.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "南投縣": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10008.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "雲林縣": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10009.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "嘉義市": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10020.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "嘉義縣": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10010.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "屏東縣": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10013.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "宜蘭縣": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10002.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "花蓮縣": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10015.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "臺東縣": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10014.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    case "澎湖縣": {
+                        HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10016.txt");
+                        CloseableHttpResponse response = httpClient.execute(httpget);
+                        HttpEntity httpEntity = response.getEntity();
+                        strResult = EntityUtils.toString(httpEntity, "utf-8");
+                        break;
+                    }
+                    default:
+                        strResult = "義大利?維大力? \nSorry 我不知道" + text + "是哪裡...";
                 }
-                case "新北市": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_65.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "桃園市": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_68.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "臺中市": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_66.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "高雄市": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_64.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "基隆市": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10017.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "新竹市": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10018.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "新竹縣": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10004.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "苗栗縣": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10005.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "彰化縣": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10007.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "南投縣": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10008.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "雲林縣": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10009.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "嘉義市": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10020.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "嘉義縣": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10010.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "屏東縣": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10013.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "宜蘭縣": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10002.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "花蓮縣": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10015.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "臺東縣": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10014.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                case "澎湖縣": {
-                    HttpGet httpget = new HttpGet("http://www.cwb.gov.tw/V7/forecast/taiwan/Data/W50_10016.txt");
-                    CloseableHttpResponse response = httpClient.execute(httpget);
-                    HttpEntity httpEntity = response.getEntity();
-                    strResult = EntityUtils.toString(httpEntity, "utf-8");
-                    break;
-                }
-                default:
-                    strResult = "義大利?維大力?";
+                strResult = strResult.replace("<BR><BR>", "\n");
+                strResult = strResult.replaceAll("<[^<>]*?>", "");
+                this.replyText(replyToken, strResult);
             }
-            strResult = strResult.replace("<BR><BR>", "\n");
-            strResult = strResult.replaceAll("<[^<>]*?>", "");
-            this.replyText(replyToken, strResult);
         }
+        this.replyText(replyToken, text);
     }
 
     private void replyText(@NonNull String replyToken, @NonNull String message) {
